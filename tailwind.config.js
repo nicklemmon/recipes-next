fef7f4990226bee139c848c7486d10ef29b83f05
Nick Lemmon <nick.lemmon@gmail.com>
2020-11-21 15:58:42 -0500

    Install Tailwind V2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: ['./src/components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
