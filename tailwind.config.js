1d04ba270703b0599df3546f30f4aa99a5f9374a
Nick Lemmon <nick.lemmon@gmail.com>
2020-11-22 10:55:05 -0500

    Fix purging bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./src/components/**/*.js'],
+  purge: ['./src/components/**/*.js', './src/pages/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
