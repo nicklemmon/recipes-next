bdb95bd502e741d10b6dae7c922efcf5c9187021
Nick Lemmon <nick.lemmon@gmail.com>
2020-11-23 15:45:11 -0500

    main - Update CardLink styles

--- src/components/links/CardLink.js
+++ src/components/links/CardLink.js
@@ -4,15 +4,15 @@ import { PageLink } from 'src/components/links'
 
 export default function CardLink({ as = PageLink, href, children, className }) {
   const Component = as
-  const hoverStyles = 'hover:shadow hover:text-blue-500'
+  const hoverStyles = 'hover:shadow hover:text-blue-500 hover:border-indigo-200'
   const focusStyles =
-    'hover:shadow focus:text-blue-500 focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50'
+    'hover:shadow focus:text-blue-500 focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50 hover:border-indigo-200'
 
   return (
     <Component
       href={href}
       className={classNames(
-        `flex flex-col items-center justify-center w-full h-full text-center text-gray-600 no-underline p-16 bg-white shadow-lg rounded-lg outline-none ${hoverStyles} ${focusStyles}`,
+        `flex flex-col items-center justify-center w-full h-full text-center border border-indigo-100 text-gray-600 no-underline p-16 bg-white shadow-lg rounded-lg outline-none ${hoverStyles} ${focusStyles}`,
         className
       )}
     >
