5d304624b1231feabec6a89d06398d97c97df6d0
Nick Lemmon <nick.lemmon@gmail.com>
2021-05-30 21:14:53 -0400

    Pass more props to Icon wrapper el

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export function Icon({ as, label, size, fill = 'inherit' }) {
+export function Icon({ as, className, label, size, fill = 'inherit' }) {
   const role = label ? 'img' : 'presentation'
   const ariaLabel = label ? label : null
   const styles = {
@@ -9,7 +9,12 @@ export function Icon({ as, label, size, fill = 'inherit' }) {
   }
 
   return (
-    <span role={role} aria-label={ariaLabel} style={styles}>
+    <span
+      className={className}
+      role={role}
+      aria-label={ariaLabel}
+      style={styles}
+    >
       {as}
     </span>
   )
