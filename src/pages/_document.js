c0aded175cf1a843ab9ea9efe99fa5489b1975c3
Nick Lemmon <nick.lemmon@gmail.com>
2022-06-15 14:07:11 -0400

    Adds custom favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥘</text></svg>"
+          />
+        </Head>
         <body>
           <Main />
           <div id={PORTAL_IDS.MODAL} />
